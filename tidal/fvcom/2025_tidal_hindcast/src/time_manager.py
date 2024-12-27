69b4764715f02ceb2f2355a9a6496f3724f88279
Andrew Simms <andrew.simms@nrel.gov>
2024-12-27 13:56:46 -0700

    Feat: Add initial version of time management

---
+++ tidal/fvcom/2025_tidal_hindcast/src/time_manager.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+
+TIME_VARIABLE = "Times"
+
+
+def standardize_fvcom_time(ds):
+    # Convert string times to datetime64
+    times_string = [ts.decode("utf-8") for ts in ds[TIME_VARIABLE].values]
+    std_datetimes = pd.to_datetime(times_string, utc=True)
+    return {
+        "original": ds["time"].values,
+        "Timestamp": std_datetimes,
+        "datetime64[ns]": std_datetimes.values,
+    }
+
+
+def does_time_always_increase(time_array):
+    this_series = pd.Series(time_array)
+    return this_series.is_monotonic_increasing
+
+
+def does_time_have_duplicates(time_array):
+    this_series = pd.Series(time_array)
+    return this_series.duplicated().any()
+
+
+def calculate_time_delta_seconds(timestamps: pd.DatetimeIndex):
+    deltas = timestamps.diff().dropna()
+    delta_seconds = deltas.dt.total_seconds()
+
+    return {
+        "mean_dt": delta_seconds.mean(),
+        "min_dt": delta_seconds.min(),
+        "max_dt": delta_seconds.max(),
+    }
