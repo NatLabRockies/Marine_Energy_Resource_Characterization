fa55aed6ad8a4377a5607e6c6b4085ca3bfbca0f
Andrew Simms <andrew.simms@nrel.gov>
2024-12-27 13:44:05 -0700

    Feat: Add first version of processing entry point, runner.py

---
+++ tidal/fvcom/2025_tidal_hindcast/runner.py
@@ -0,0 +1,21 @@
+from config import config
+from src.cli import parse_args
+from src.file_manager import get_specified_nc_files
+from src.verify import verify_dataset
+
+
+if __name__ == "__main__":
+    args = parse_args(config)
+
+    # Access the location config
+    location = config["location_specification"][args.location]
+    output_type = args.output_type
+
+    print(f"Standardizing {location} tidal dataset for output type {output_type}....")
+
+    print("Finding nc files...")
+    nc_files = get_specified_nc_files(location)
+    print(f"Found {len(nc_files)} files!")
+
+    print("Step 1: Verifying Dataset Integrity...")
+    verify_dataset(config, location, nc_files)
