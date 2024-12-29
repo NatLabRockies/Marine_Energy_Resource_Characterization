08dc03d9e03ab2e6f24e70a464830c296e78c3d6
Andrew Simms <andrew.simms@nrel.gov>
2024-12-29 10:26:28 -0700

    Feat: Remove non user centric variables from output datasets

--- tidal/fvcom/2025_tidal_hindcast/src/standardize.py
+++ tidal/fvcom/2025_tidal_hindcast/src/standardize.py
@@ -1,6 +1,48 @@
 # Create one giant dataset using the valid_timestamps
+import xarray as xr
 
 from . import time_manager
+
+
+def remove_non_user_centric_variables(ds, config):
+    variable_spec = config["model_specification"]
+    user_centric_variable_names = list(variable_spec.keys())
+
+    # Get variables to drop - those in dataset but not in specification
+    variables_to_drop = [
+        var for var in ds.variables if var not in user_centric_variable_names
+    ]
+
+    # Remove coordinate variables from the drop list to preserve dataset structure
+    coord_vars = list(ds.coords)
+    variables_to_drop = [var for var in variables_to_drop if var not in coord_vars]
+
+    # Drop non-user variables while preserving coordinates
+    return ds.drop_vars(variables_to_drop)
+
+
+def standardize_time(ds, pandas_utc_timestamps):
+    new_time = xr.DataArray(
+        pandas_utc_timestamps,
+        dims="time",
+        name="time",
+        attrs={
+            "long_name": "Time",
+            "standard_name": "time",
+            "timezone": "UTC",
+            "data_source": "derived",
+            "data_source_description": "Derived from `Times` variable string. Original data `time` is in non standard modified julian timestamps",
+            # ValueError: failed to prevent overwriting existing key units in attrs on variable 'time'. This is probably an encoding field used by xarray to describe how a variable is serialized. To proceed, remove this key from the variable's attributes manually.
+            # "units": "seconds since 1970-01-01 00:00:00",
+        },
+    )
+    ds = ds.drop_vars("time")
+    ds = ds.assign_coords(time=new_time)
+    return ds
+
+
+def standardize_coords(ds):
+    return ds
 
 
 def standardize_dataset(config, location, nc_files, valid_timestamps_df):
