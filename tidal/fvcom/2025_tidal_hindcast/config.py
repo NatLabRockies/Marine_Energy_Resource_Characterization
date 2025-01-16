2062cb50081ebb8ea1ea72c67b6e2a2e71cb9179
Andrew Simms <andrew.simms@nrel.gov>
2025-01-15 18:19:25 -0700

    Config: Completely rewrite global attributes to follow CF conventions

--- tidal/fvcom/2025_tidal_hindcast/config.py
+++ tidal/fvcom/2025_tidal_hindcast/config.py
@@ -489,12 +489,213 @@ config = {
             "partition_frequency": "M",  # Monthly, Roughly 50GB per file
         },
     },
-    "metadata": {
-        "code_url": "https://github.com/nrel/marine_energy_resource_characterization/tidal/fvcom",
-        "cf_conventions": "1.0",
-        "conventions": "ME Data Pipeline Standards v1.0",
-        "data_generation_organization": "Pacific Northwest National Laboratory (PNNL)",
-        "data_processing_organization": "National Renewable Energy Laboratory (NREL)",
-        "funding_organization": "United States Department of Energy Water Power Technologies Office",
+    "global_attributes": {
+        # Global attributes that will be recorded in the output dataset in `attrs` (attributes). These metadata are
+        # used to record data provenance information (e.g., location, institution, etc),
+        # construct datastream and file names (i.e., location_id, dataset_name, qualifier,
+        # temporal, and data_level attributes), as well as provide metadata that is useful for
+        # data users (e.g., title, description, ... ).
+        # Source: ACDD
+        # A comma-separated list of the conventions that are followed by the dataset.
+        # For files that follow this version of ACDD, include the string 'ACDD-1.3'.
+        "Conventions": "CF-1.0, ACDD-1.3, ME Data Pipeline-1.0",
+        # Source: ACDD
+        # A place to acknowledge various types of support for the project that produced this data.
+        "acknowledgement": "This work was funded by the U.S. Department of Energy, Office of "
+        "Energy Efficiency & Renewable Energy, Water Power Technologies Office. The authors "
+        "gratefully acknowledge project support from Heather Spence and Jim McNally (U.S. "
+        "Department of Energy Water Power Technologies Office) and Mary Serafin (National "
+        "Renewable Energy Laboratory). Technical guidance was provided by Vincent Neary (Sandia "
+        "National Laboratories), Levi Kilcher, Caroline Draxl, and Katie Peterson (National "
+        "Renewable Energy Laboratory).",
+        # Source: IOOS
+        # Country of the person or organization that operates a platform or network,
+        # which collected the observation data.
+        "creator_country": "USA",
+        # Source: ACDD, IOOS
+        # The email address of the person (or other creator type specified by the creator_type
+        # attribute) principally responsible for creating this data.
+        "creator_email": "zhaoqing.yang@pnnl.gov",
+        # Source: ACDD, IOOS
+        # The institution of the creator; should uniquely identify the creator's institution.
+        # This attribute's value should be specified even if it matches the value of
+        # publisher_institution, or if creator_type is institution.
+        "creator_institution": "Pacific Northwest National Laboratory (PNNL)",
+        # Source: IOOS
+        # URL for the institution that collected the data. For clarity, it is recommended
+        # that this field is specified even if the creator_type is institution and a
+        # creator_url is provided.
+        "creator_institution_url": "https://www.pnnl.gov/",
+        # Source: ACDD
+        # The name of the person (or other creator type specified by the creator_type
+        # attribute) principally responsible for creating this data.
+        "creator_name": "Zhaoqing Yang",
+        # Source: IOOS
+        # IOOS classifier (https://mmisw.org/ont/ioos/sector) that best describes
+        # the platform (network) operator's societal sector.
+        "creator_sector": "gov_federal",
+        # Source: IOOS
+        # State or province of the person or organization that collected the data.
+        "creator_state": "Washington",
+        # Source: ACDD
+        # Specifies type of creator with one of the following: 'person', 'group',
+        # 'institution', or 'position'. If this attribute is not specified, the
+        # creator is assumed to be a person.
+        "creator_type": "institution",
+        # Source: ACDD, IOOS
+        # The URL of the person (or other creator type specified by the creator_type
+        # attribute) principally responsible for creating this data.
+        "creator_url": "https://www.pnnl.gov/",
+        # Source: ACDD
+        # The name of any individuals, projects, or institutions that contributed to
+        # the creation of this data.
+        "contributor_name": "Mithun Deb, Andrew Simms, Ethan Young",
+        # Source: ACDD
+        # The role of any individuals, projects, or institutions that contributed to
+        # the creation of this data.
+        # Contributor Role Definitions: "https://vocab.nerc.ac.uk/collection/G04/current/".
+        # | Role                    | Definition                                                                                                       |
+        # | ----------------------- | ---------------------------------------------------------------------------------------------------------------- |
+        # | author                  | Party who authored the resource                                                                                  |
+        # | coAuthor                | Party who jointly authors the resource                                                                           |
+        # | collaborator            | Party who assists with the generation of the resource other than the principal investigator                      |
+        # | contributor             | Party contributing to the resource                                                                               |
+        # | custodian               | Party that accepts accountability and responsibility for the data, and ensures appropriate care and maintenance  |
+        # | distributor             | Party who distributes the resource                                                                               |
+        # | editor                  | Party who reviewed or modified the resource to improve the content                                               |
+        # | funder                  | Party providing monetary support for the resource                                                                |
+        # | mediator                | A class of entity that mediates access to the resource and for whom the resource is intended or useful           |
+        # | originator              | Party who created the resource                                                                                   |
+        # | owner                   | Party that owns the resource                                                                                     |
+        # | pointOfContact          | Party who can be contacted for acquiring knowledge about or acquisition of the resource                          |
+        # | principalInvestigator   | Key party responsible for gathering information and conducting research                                          |
+        # | processor               | Party that has processed the data in a manner such that the resource has been modified                           |
+        # | publisher               | Party who published the resource                                                                                 |
+        # | resourceProvider        | Party that supplies the resource                                                                                 |
+        # | rightsHolder            | Party owning or managing rights over the resource                                                                |
+        # | sponsor                 | Party who speaks for the resource                                                                                |
+        # | stakeholder             | Party who has an interest in the resource or the use of the resource                                             |
+        # | user                    | Party who uses the resource                                                                                      |
+        "contributor_role": "author, processor, processor",
+        # Source: IOOS
+        # The URL of the controlled vocabulary used for the contributor_role attribute.
+        # The default is "https://vocab.nerc.ac.uk/collection/G04/current/".
+        "contributor_role_vocabulary": "https://vocab.nerc.ac.uk/collection/G04/current/",
+        # Source: IOOS
+        # The URL of the individuals or institutions that contributed to the creation
+        # of this data.
+        "contributor_url": "https://www.pnnl.gov, www.nrel.gov",
+        # Source: Global, ACDD
+        # A user-friendly description of the dataset. It should provide enough context
+        # about the data for new users to quickly understand how the data can be used.
+        "description": None,
+        # Source: Global
+        # The DOI that has been registered for this dataset, if applicable.
+        "doi": None,
+        # Source: Global, ACDD, IOOS
+        # CF attribute for identifying the featureType.
+        # https://cfconventions.org/Data/cf-conventions/cf-conventions-1.12/cf-conventions.html#table-feature-types
+        "featureType": "timeSeries",
+        # Source: IOOS
+        # URL for background information about this dataset.
+        "infoURL": "https://www.github.com/nrel/marine_energy_resource_characterization/tidal/fvcom/fy_25_tidal_hindcast",
+        # Source: ACDD
+        # Name of the contributing instrument(s) or sensor(s) used to create this
+        # data set or product.
+        "instrument": None,
+        # Source: ACDD
+        # Controlled vocabulary for the names used in the 'instrument' attribute.
+        "instrument_vocabulary": None,
+        # Source: ACDD
+        # A comma-separated list of key words and/or phrases.
+        # "keywords": "OCEAN WAVES, GRAVITY WAVES, WIND WAVES, SIGNIFICANT WAVE HEIGHT, WAVE FREQUENCY, WAVE PERIOD, WAVE SPECTRA,",
+        "keywords": "OCEAN TIDES, TIDAL ENERGY, VELOCITY, SPEED, DIRECTION, POWER DENSITY",
+        # Source: ACDD
+        # If you are using a controlled vocabulary for the words/phrases in your
+        # 'keywords' attribute, this is the unique name or identifier of the
+        # vocabulary from which keywords are taken.
+        # Excluded by PNNL
+        "keywords_vocabulary": None,
+        # Source: ACDD, IOOS
+        # Provide the URL to a standard or specific license, enter 'Freely
+        # Distributed' or 'None', or describe any restrictions to data access
+        # and distribution in free text.
+        "license": "Freely Distributed",
+        # Source: Global
+        # A label or acronym for the location where the data were obtained from.
+        # Only alphanumeric characters and '_' are allowed.
+        # "location_id": "cpr",
+        # "make_model": "Sofar Spotter2",
+        # Source: ACDD, IOOS
+        # The organization that provides the initial id for the dataset.
+        # "naming_authority": "gov.pnnl.sequim",
+        "naming_authority": "gov.nrel.water_power",
+        # Source: ACDD, IOOS
+        # Name of the platform(s) that supported the sensor data used to create this
+        # data set or product.
+        # "platform": "wave_buoy",
+        # Source: IOOS
+        # An optional, short identifier for the platform, if the data provider
+        # prefers to define an id that differs from the dataset identifier.
+        # "platform_id": None,
+        # Source: IOOS
+        # A descriptive, long name for the platform used in collecting the data.
+        # "platform_name": None,
+        # Source: ACDD, IOOS
+        # Controlled vocabulary for the names used in the 'platform' attribute.
+        # "platform_vocabulary": "http://mmisw.org/ont/ioos/platform",
+        # Source: Global
+        # Optional attribute used to cite other data, algorithms, etc. as needed.
+        "references": "Spicer, P., Z. Yang, T. Wang, and M. Deb. (in prep.)."
+        "Considering the relative importance of diurnal and semidiurnal tides "
+        "in tidal power potential around the Aleutian Islands, AK, Renewable Energy.",
+        # Source: Global
+        # An optional string which distinguishes these data from other datasets
+        # produced by the same instrument.
+        # "qualifier": None,
+        # Source: ACDD
+        # The data type, as derived from Unidata's Common Data Model Scientific Data types.
+        # "cdm_data_type": None,
+        # Source: ACDD
+        # Miscellaneous information about the data, not captured elsewhere.
+        "comment": None,
+        # Source: ACDD
+        # A URL that gives the location of more complete metadata.
+        "metadata_link": None,
+        # Source: ACDD
+        # The overarching program(s) of which the dataset is a part.
+        "program": "DOE Water Power Technologies Office Marine Energy Resource Characterization",
+        # Source: ACDD
+        # The name of the project(s) principally responsible for originating
+        # this data.
+        "project": "High Resolution Tidal Hindcast",
+        # Source: ACDD
+        # A paragraph describing the dataset, analogous to an abstract for a paper.
+        "summary": None,
+        # Source: IOOS
+        # Country of the person or organization that distributes the data.
+        "publisher_country": "USA",
+        # Source: ACDD, IOOS
+        # The email address of the person responsible for publishing the data file
+        # or product to users.
+        "publisher_email": "michael.lawson@nrel.gov",
+        # Source: ACDD, IOOS
+        # The institution of the publisher.
+        "publisher_institution": "Pacific Northwest National Laboratory (PNNL)",
+        # Source: ACDD
+        # The name of the person responsible for publishing the data file or
+        # product to users.
+        "publisher_name": "Michael Lawson",
+        # Source: IOOS
+        # State or province of the person or organization that distributes the data.
+        "publisher_state": "Colorado",
+        # Source: ACDD
+        # Specifies type of publisher with one of the following: 'person', 'group',
+        # 'institution', or 'position'.
+        "publisher_type": "institution",
+        # Source: ACDD, IOOS
+        # The URL of the person responsible for publishing the data file or product
+        # to users.
+        "publisher_url": "https://www.nrel.gov",
     },
 }
