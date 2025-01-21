64ca9a8d19ef2ec7a132286bdad0e2a82aa1464e
Andrew Simms <andrew.simms@nrel.gov>
2025-01-21 16:29:30 -0700

    VAP: Add early exit if expected number of files exists in output dir

--- tidal/fvcom/2025_tidal_hindcast/src/derive_vap_fvcom.py
+++ tidal/fvcom/2025_tidal_hindcast/src/derive_vap_fvcom.py
@@ -778,6 +778,16 @@ def derive_vap(config, location_key):
     )
     std_partition_nc_files = sorted(list(std_partition_path.rglob("*.nc")))
 
+    vap_output_dir = Path(file_manager.get_vap_output_dir(config, location))
+
+    existing_vap_nc_files = sorted(list(vap_output_dir.rglob("*.nc")))
+
+    if len(existing_vap_nc_files) < 12:
+        print(
+            f"Found {len(existing_vap_nc_files)} files in {vap_output_dir}. Skipping derive vap!"
+        )
+        return
+
     for count, nc_file in enumerate(std_partition_nc_files, start=1):
         print(f"Calculating vap for {nc_file}")
         this_ds = xr.open_dataset(nc_file)
