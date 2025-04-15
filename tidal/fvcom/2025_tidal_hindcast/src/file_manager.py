e6d31ac0341791fd459bb6efe6c47c41ff26c27d
Simms, Andrew <Andrew.Simms@nrel.gov>
2025-04-14 21:18:52 -0600

    File Manager: Convert data dir to string

--- tidal/fvcom/2025_tidal_hindcast/src/file_manager.py
+++ tidal/fvcom/2025_tidal_hindcast/src/file_manager.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 def get_specified_nc_files(config, location):
     original_data_dir = Path(config["dir"]["input"]["original"]).resolve()
-    original_data_dir = original_data_dir.replace("<location>", location["output_name"])
+    original_data_dir = str(original_data_dir).replace(
+        "<location>", location["output_name"]
+    )
     search_dir = Path(original_data_dir, location["base_dir"]).resolve()
     if search_dir.exists() is not True:
         raise ValueError(f"Directory Error: {search_dir} does not exist!")
