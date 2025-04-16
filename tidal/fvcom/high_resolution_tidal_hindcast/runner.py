15c36d52caa7c239412808327de1b30186ef2eeb
Simms, Andrew <Andrew.Simms@nrel.gov>
2025-04-16 13:35:41 -0600

    Runner: Enable parquet partition

--- tidal/fvcom/high_resolution_tidal_hindcast/runner.py
+++ tidal/fvcom/high_resolution_tidal_hindcast/runner.py
@@ -49,8 +49,8 @@ if __name__ == "__main__":
     print("Step 6: Calculating Yearly Average...")
     calculate_vap_yearly_average(config, args.location)
 
-    # print("Step 7: Create Parquet Partition Dataset...")
-    # partition_vap_into_parquet_dataset(config, args.location)
+    print("Step 7: Create Parquet Partition Dataset...")
+    partition_vap_into_parquet_dataset(config, args.location)
 
     print("Step 8: Create Summary Parquet Dataset...")
     convert_nc_summary_to_parquet(config, args.location)
