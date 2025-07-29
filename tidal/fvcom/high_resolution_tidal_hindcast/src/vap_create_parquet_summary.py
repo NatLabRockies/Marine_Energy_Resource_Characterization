9399daabaa7c3fe99254cbef6bd48f3fc7ba6991
Simms, Andrew <Andrew.Simms@nrel.gov>
2025-07-29 10:05:03 -0600

    Summary: Fix, use correct function arg name

--- tidal/fvcom/high_resolution_tidal_hindcast/src/vap_create_parquet_summary.py
+++ tidal/fvcom/high_resolution_tidal_hindcast/src/vap_create_parquet_summary.py
@@ -797,7 +797,7 @@ def convert_nc_summary_to_parquet(
             ext=None,
             version=config["dataset"]["gis_output_version"],
             include_creation_timestamp=True,
-            include_dataset_time=False,
+            include_dataset_timestamp=False,
         )
 
         # Archive existing files in complete directory before creating new ones
