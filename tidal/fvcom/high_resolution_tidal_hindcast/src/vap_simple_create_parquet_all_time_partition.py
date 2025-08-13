f0aec4d45d3afe47cf73c2a027487d94d309f5bc
Simms, Andrew <Andrew.Simms@nrel.gov>
2025-08-12 21:57:08 -0600

    Parquet Partition: Skip "nv" data for output dataframe

--- tidal/fvcom/high_resolution_tidal_hindcast/src/vap_simple_create_parquet_all_time_partition.py
+++ tidal/fvcom/high_resolution_tidal_hindcast/src/vap_simple_create_parquet_all_time_partition.py
@@ -564,7 +564,7 @@ def convert_h5_to_parquet_batched(
 
         # Convert all lists to numpy arrays and handle constant values
         for key, value in all_face_data[face_id].items():
-            if "nv_layer" in key:
+            if "nv" in key:
                 continue
             if key in [
                 "lat",
