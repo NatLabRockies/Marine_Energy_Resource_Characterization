ef7fb1e3b9db83e9e3953e6c7bcfd2c15f421c70
Simms, Andrew <Andrew.Simms@nrel.gov>
2025-11-17 12:42:35 -0700

    Runner: Enable step 2 only

--- tidal/fvcom/high_resolution_tidal_hindcast/runner.py
+++ tidal/fvcom/high_resolution_tidal_hindcast/runner.py
@@ -44,18 +44,18 @@ if __name__ == "__main__":
     location = config["location_specification"][args.location]
     output_type = args.output_type
 
-    print(f"Standardizing {location} tidal dataset for output type {output_type}....")
-
-    print("Finding nc files...")
-    try:
-        nc_files = get_specified_nc_files(config, location)
-        print(f"Found {len(nc_files)} files!")
-    except PermissionError:
-        print("Permissions error accessing original files")
-        nc_files = []
-
-    print("Step 1: Verifying Dataset Integrity...")
-    valid_timestamps_df = verify_dataset(config, location, nc_files)
+    # print(f"Standardizing {location} tidal dataset for output type {output_type}....")
+    #
+    # print("Finding nc files...")
+    # try:
+    #     nc_files = get_specified_nc_files(config, location)
+    #     print(f"Found {len(nc_files)} files!")
+    # except PermissionError:
+    #     print("Permissions error accessing original files")
+    #     nc_files = []
+    #
+    # print("Step 1: Verifying Dataset Integrity...")
+    # valid_timestamps_df = verify_dataset(config, location, nc_files)
 
     tracking_folder = get_tracking_output_dir(config, location)
     tracking_path = Path(
@@ -63,10 +63,10 @@ if __name__ == "__main__":
     )
     valid_timestamps_df = pd.read_parquet(tracking_path)
 
-    # print("Step 2: Modifying Original Dataset to Create a Standardized Dataset...")
-    # valid_std_files_df = standardize_dataset(
-    #     config, args.location, valid_timestamps_df, skip_if_verified=False
-    # )
+    print("Step 2: Modifying Original Dataset to Create a Standardized Dataset...")
+    valid_std_files_df = standardize_dataset(
+        config, args.location, valid_timestamps_df, skip_if_verified=False
+    )
     #
     # print("Step 3: Partitioning Standardized Dataset by Time...")
     # partition_by_time(config, args.location, valid_std_files_df)
