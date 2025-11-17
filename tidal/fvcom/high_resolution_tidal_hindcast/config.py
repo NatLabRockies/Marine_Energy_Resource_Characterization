79a1e2841827183a9e3c082459dba521ee2a910e
Simms, Andrew <Andrew.Simms@nrel.gov>
2025-11-16 20:44:01 -0700

    Config: Set high level version to 1.0.0

--- tidal/fvcom/high_resolution_tidal_hindcast/config.py
+++ tidal/fvcom/high_resolution_tidal_hindcast/config.py
@@ -4,7 +4,7 @@ config = {
     "dataset": {
         "label": "High Resolution Tidal Hindcast",
         "name": "wpto_high_res_tidal",
-        "version": "0.4.0",
+        "version": "1.0.0",
         "gis_output_version": "1.0.0",
         "development_version": "1.0.0",
         "issue_date": "2025-11-12",
