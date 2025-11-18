a59cf3189ccd6c6f35de969d1d2430781e957a91
Simms, Andrew <Andrew.Simms@nrel.gov>
2025-11-18 13:02:55 -0700

    Runner: Don't skip step 2 if files exist

--- tidal/fvcom/high_resolution_tidal_hindcast/runner.py
+++ tidal/fvcom/high_resolution_tidal_hindcast/runner.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
 
     print("Step 2: Modifying Original Dataset to Create a Standardized Dataset...")
     valid_std_files_df = standardize_dataset(
-        config, args.location, valid_timestamps_df, skip_if_verified=True
+        config, args.location, valid_timestamps_df, skip_if_verified=False
     )
 
     print("Step 3: Partitioning Standardized Dataset by Time...")
